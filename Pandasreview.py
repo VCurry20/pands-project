3fba11e98371e1b62bd09fa9bd6a6593a6c21f1e
Curry <G00074924@gmit.ie>
2021-04-18 16:38:31 +0100

    Analysis

--- Pandasreview.py
+++ Pandasreview.py
@@ -6,4 +6,14 @@ import numpy as np
 filename = 'kaggleIrisSet.csv'
 df = pd.read_csv(filename)
 
-# print(df.describe())
+
+#df.to_csv("Dataprintout.csv")                        # Print out to a CSV file - this could also be an excel file etc
+
+# print(df.describe())                                # Breakdown of the csv - outputs a review of the numerical data
+# print(df.describe(include='all'))                   # Outputs all the data
+# print (df.SepalLengthCm.describe())                 # Outputs just the SepalLengthCm Column
+# print(df.SepalWidthCm.describe())                   # Outputs just the SepalWidthCm Column
+# print(df.PetalLengthCm.describe())                  # Outputs just the PetalLengthCm Column
+# print(df.PetalWidthCm.describe())                   # Outputs just the PetalWidthCm Column
+print(df.describe(exclude=[np.Species]))
+# print(df.describe()
