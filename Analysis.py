a6f3906698740e14fe36ff84da796827bce89a26
Curry <G00074924@gmit.ie>
2021-04-23 18:45:15 +0100

    Analysis

--- Analysis.py
+++ Analysis.py
@@ -27,7 +27,7 @@ irisGroup = df.groupby("Species").size()            # Set requirement to group b
 # Some lines include more code / some print variables set above 
 # The following will print the output with new lines between results \n
 
-with open("Analysis1output.txt", "wt") as f:                                                   # Open File "Analysis1out" as a txt file, in write txt mode
+with open("Analysisoutput.txt", "wt") as f:                                                   # Open File "Analysis1out" as a txt file, in write txt mode
     print("\nThe full breakdown is: \n", df.describe(), file=f)                                # Breakdown of the csv - outputs a review of the numerical data
     print("\nThe Sepal Length Column only is: \n", df.SepalLengthCm.describe(), file=f)        # Outputs just the SepalLengthCm Column
     print("\nThe Sepal Width Column only is: \n",df.SepalWidthCm.describe(), file=f)           # Outputs just the SepalWidthCm Column
@@ -43,7 +43,7 @@ import io                                                                       
 buffer = io.StringIO()
 df.info(buf=buffer)
 i = buffer.getvalue()
-with open("Analysis1output.txt", "a") as f:
+with open("Analysisoutput.txt", "a") as f:
     f.write("\nThis is an overview of the file information: \n{}".format(i))
 
 # https://stackoverflow.com/questions/64067424/how-to-convert-df-info-into-data-frame-df-info
