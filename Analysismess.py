f5c6e5ebfd0255b501b9d51c15e363e45d7f1bd3
Curry <G00074924@gmit.ie>
2021-04-23 17:13:27 +0100

    Analysis

--- Analysismess.py
+++ Analysismess.py
@@ -1,3 +1,16 @@
+# just to ck outputs not for submission
+
+# Reviewing the Data using Pandas
+
+print ("Hello World")
+
+import pandas as pd 
+import numpy as np 
+
+filename = 'kaggleIrisSet.csv'
+df = pd.read_csv(filename)
+
+
 # Author Veronica Curry
 # Student ID: G00074924
 # Completed: April 2021
@@ -16,10 +29,7 @@ df = pd.read_csv(filename)
 df.to_excel("Dataprintout.xlsx")                    # Print out to a CSV file - this could also be an excel file etc
 
 
-# setting variables for printing below
-irisData = pd.read_csv("kaggleIrisSet.csv")    
-irisGroup = irisData.groupby("Species").size()
-irisCorr = irisData.corr()
+
 
 
 
@@ -30,8 +40,14 @@ with open("Analysis1output.txt", "wt") as f:                                    
     print("\nThe Sepal Width Column only is: \n",df.SepalWidthCm.describe(), file=f)           # Outputs just the SepalWidthCm Column
     print("\nThe Petal Length Column only is: \n",df.PetalLengthCm.describe(), file=f)         # Outputs just the PetalLengthCm Column
     print("\nThe Petal Width Column only is: \n",df.PetalWidthCm.describe(), file=f)           # Outputs just the PetalWidthCm Column
-    print("\nThe top lines of the data are as follows: \n",irisData.head(), file=f)            # Outputs the top rows of the data
-    print("\nThe Data grouped by Species is: \n",irisGroup, file=f)                            # Outputs the data grouped by Species
-    print("\nThe Pairwise correlation of colums is: \n",irisCorr, file=f)                      # Outputs the data in a 
 
 
+
+irisData = pd.read_csv("kaggleIrisSet.csv")
+print (irisData.head())
+
+irisGroup = irisData.groupby("Species").size()
+print (irisGroup)
+
+irisCorr = irisData.corr()
+print (irisCorr)
